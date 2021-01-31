0dedd1261367f67909cf4305a38001847bf7fd95
leekihoon <leekihoon1689@gmail.com>
2021-02-01 00:18:17 +0900

    210131

---
+++ src/백준/no3053.java
@@ -0,0 +1,17 @@
+package น้มุ;
+
+import java.math.*;
+import java.util.Scanner;
+public class no3053 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		double R = scan.nextDouble();
+		
+		System.out.println(R * R * Math.PI);
+		
+		System.out.println(2 * R * R);
+	}
+
+}
