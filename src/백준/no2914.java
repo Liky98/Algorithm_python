1a24a6f904924cd5a40a39543c69d46488275020
leekihoon <leekihoon1689@gmail.com>
2021-02-07 22:40:07 +0900

    210207

---
+++ src/백준/no2914.java
@@ -0,0 +1,15 @@
+package น้มุ;
+
+import java.util.Scanner;
+
+public class no2914 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		System.out.println(a* (b-1) +1);
+	}
+
+}
