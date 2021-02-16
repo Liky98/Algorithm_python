a4b2e0899f2dd676d49e2c44199df6213ff48ddf
leekihoon <leekihoon1689@gmail.com>
2021-02-16 22:33:21 +0900

    210216

---
+++ src/백준/no15649.java
@@ -0,0 +1,14 @@
+package น้มุ;
+
+import java.util.Scanner;
+
+public class no15649 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int m = scan.nextInt();
+		int n = scan.nextInt();
+		
+	}
+
+}
---
+++ src/백준/no5338.java
@@ -0,0 +1,18 @@
+package น้มุ;
+
+public class no5338 {
+
+	public static void main(String[] args) {
+//	       _.-;;-._
+//	       '-..-'|   ||   |
+//	       '-..-'|_.-;;-._|
+//	       '-..-'|   ||   |
+//	       '-..-'|_.-''-._|
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+
+}
