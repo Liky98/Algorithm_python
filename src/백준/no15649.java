ce8bc57d964d735e37f7c471188fd742bbb6584a
leekihoon <leekihoon1689@gmail.com>
2021-02-17 17:41:06 +0900

    210217

--- src/백준/no15649.java
+++ src/백준/no15649.java
@@ -3,12 +3,40 @@ package น้มุ;
 import java.util.Scanner;
 
 public class no15649 {
-
+ 
+	public static int[] arr;
+	public static boolean[] visit;
+ 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int m = scan.nextInt();
-		int n = scan.nextInt();
-		
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+		int M = in.nextInt();
+ 
+		arr = new int[M];
+		visit = new boolean[N];
+		dfs(N, M, 0);
+ 
 	}
-
+ 
+	public static void dfs(int N, int M, int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				System.out.print(val + " ");
+			}
+			System.out.println();
+			return;
+		}
+ 
+		for (int i = 0; i < N; i++) {
+			if (!visit[i]) {
+				visit[i] = true;
+				arr[depth] = i + 1;
+				dfs(N, M, depth + 1);
+				visit[i] = false;
+			}
+		}
+	}
+ 
 }
