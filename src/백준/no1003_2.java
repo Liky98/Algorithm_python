b02423e1a5aba2dbbc78f94a33f7f596f2c74319
leekihoon <leekihoon1689@gmail.com>
2021-03-03 16:21:43 +0900

    210303

---
+++ src/백준/no1003_2.java
@@ -0,0 +1,26 @@
+package น้มุ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class no1003_2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(bfr.readLine());
+		for(int i=0; i<T; i++) {
+			int num = Integer.parseInt(bfr.readLine());
+			int zero = 1;
+			int one = 0;
+			int temp;
+			for(int j=0; j<num; j++) {
+				temp = one;
+				one = zero + one;
+				zero = temp;
+			}
+			System.out.println(zero + " " + one);
+		}
+	}
+
+}
